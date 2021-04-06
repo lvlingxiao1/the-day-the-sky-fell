00594256d1c7481dfc6222106f58e9c52d57a611
Jackie <jackie.anj.liu@gmail.com>
2021-04-06 12:02:40 -0400

    add trail files

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -17,7 +17,7 @@ public class PauseMenu : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.F2)) {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
             if (paused) {
                 Resume();
             } else {
